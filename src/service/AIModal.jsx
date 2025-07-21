45150ca0e5a28fae38c23e64b72541163e44d6ef
Rahul-kal <127105474+Rahul-kal@users.noreply.github.com>
2025-07-21 22:20:28 +0530

    Update AIModal.jsx

--- src/service/AIModal.jsx
+++ src/service/AIModal.jsx
@@ -1,22 +1,23 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-  
-  const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_API_KEY;
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-  });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-    responseMimeType: "application/json",
-  };
-  
-   export const chatSession = model.startChat({
+// Ensure your API key is correctly set in .env (VITE_GOOGLE_GEMINI_AI_API_KEY)
+const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-lite",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "application/json",
+};
+
+  export const chatSession = model.startChat({
       generationConfig,
       history: [
         {
